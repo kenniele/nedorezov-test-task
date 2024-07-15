e1fd4a4b061eb86e439cdf20524bbd0c852b3be7
kenniele <kenniele@yandex.com>
2024-07-15 23:20:42 +0300

    add front + fix back

--- account.go
+++ account.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"database/sql"
+	"errors"
+	"log"
+)
+
 type BankAccount interface {
 	Deposit(amount float64) error
 	Withdraw(amount float64) error
@@ -7,11 +13,26 @@ type BankAccount interface {
 }
 
 type Account struct {
-	balance float64
+	ID      int     `json:"ID,-"`
+	Balance float64 `json:"Balance"`
 }
 
-func (account Account) Create() error {
-	return nil
+func (account Account) Create() (int, error) {
+	row := connection.QueryRow(`SELECT * FROM "Account" WHERE "id" = $1`, account.ID)
+
+	if err := row.Scan(&account.ID, &account.Balance); errors.Is(err, sql.ErrNoRows) {
+		row = connection.QueryRow(`INSERT INTO "Account" ("Balance") VALUES (0)`)
+		if err := row.Scan(&account.ID, &account.Balance); err != nil {
+			return 0, err
+		}
+	} else if err != nil {
+		log.Println(err)
+		log.Println("Account already exists")
+		return account.ID, nil
+	}
+	log.Println("Account created")
+	return account.ID, nil
+
 }
 
 func (account *Account) Deposit(amount float64) error {
@@ -23,5 +44,5 @@ func (account *Account) Withdraw(amount float64) error {
 }
 
 func (account *Account) GetBalance() float64 {
-	return account.balance
+	return 0
 }
--- main.go
+++ main.go
@@ -5,8 +5,10 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"log"
 	"nedorezov-test-task/config"
+	"net/http"
 )
 
 var connection *sql.DB
@@ -18,24 +20,54 @@ func init() {
 }
 
 func main() {
+	var err error
 	conf := config.New()
-	fmt.Println(conf.HOST)
+	log.Println("Connecting to config file")
 	connectionString := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
 		conf.DB.HOST, conf.DB.PORT, conf.DB.USER, conf.DB.PASSWORD, conf.DB.DBNAME, conf.DB.SSLMODE)
 
-	if connection, err := sql.Open("postgres", connectionString); err != nil {
+	if connection, err = sql.Open("postgres", connectionString); err != nil {
 		log.Fatal(err)
 	}
+	log.Println("Connecting to database")
 
 	router := gin.Default()
-
+	router.Static("/assets/", "front/")
+	router.LoadHTMLGlob("templates/*.html")
 	router.GET("/", handlerIndex)
-
+	router.POST("/accounts", handlerAccountRegistration)
+	log.Println("Running server on port 8080")
 	_ = router.Run(":8080")
 }
 
 func handlerIndex(c *gin.Context) {
-	c.JSON(200, gin.H{
+	c.HTML(http.StatusOK, "index.html", gin.H{
 		"Error": nil,
 	})
+}
+
+func handlerAccountRegistration(c *gin.Context) {
+	var account Account
+	var id int
+	var err error
+
+	if err = c.BindJSON(&account); err != nil {
+		c.JSON(http.StatusNotImplemented, gin.H{
+			"Error": err.Error(),
+		})
+		log.Fatal(err)
+	}
+
+	if id, err = account.Create(); err != nil {
+		c.JSON(http.StatusNotImplemented, gin.H{
+			"Error": err,
+		})
+		log.Println(err)
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"Error": nil,
+		"ID":    id,
+	})
+
 }
